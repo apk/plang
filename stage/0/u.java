7222698459eb876c76128d01a8b3eecaa8700542
Andreas Krey <a.krey@gmx.de>
2007-09-15 17:07:32 +0000

    work

---
+++ stage/0/u.java
@@ -0,0 +1,20 @@
+// -*- mode: Java; c-basic-offset: 3; tab-width: 8; indent-tabs-mode: nil -*-
+
+import java.io.FileReader;
+import gloop.*;
+
+public class u {
+   public static void main (String [] a) {
+      try {
+         Tokenizer t = new Tokenizer (new FileReader ("test.in"));
+         Parser p = new Parser (t);
+         p.parse (new GlobalScope (), Tokenizer.EOF);
+      } catch (java.io.IOException e) {
+         System.out.println ("IOEx: " + e);
+         e.printStackTrace ();
+      } catch (Tokenizer.TokEx e) {
+         System.out.println ("TokEx: " + e);
+         e.printStackTrace ();
+      }
+    }
+}
